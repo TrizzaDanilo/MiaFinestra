23d38605589e3c235d643c7b12e139b5c0c6b0a2
TrizzaDanilo <trizzadanilo@gmail.com>
2025-01-13 11:29:34 +0100

    Create Applicazione.java

---
+++ Applicazione.java
@@ -0,0 +1,10 @@
+public class Applicazione(
+public static void main(String[] args)
+String 11- "North";
+String 12="Center";
+String 13-"Button";
+~S
+MiaFinestra f1-new MiaFinestra(11,12,13);
+f1.setLocation[x:12b0,y:400);
+f1.setSize(width:400,height:200);
+f1.setVisible(b:true);

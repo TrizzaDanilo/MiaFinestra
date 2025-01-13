307030abf2685a7b49dd8cf508c1554846c760ed
TrizzaDanilo <trizzadanilo@gmail.com>
2025-01-13 10:46:54 +0000

    correzzioni finali

--- Applicazione.java
+++ Applicazione.java
@@ -1,13 +1,13 @@
 public class Applicazione{
   public static void main(String[] args){
-    String 11 = "North";
-    String 12 = "Center";
-    String 13 = "Button";
+    String l1 = "North";
+    String l2 = "Center";
+    String l3 = "Button";
 
-MiaFinestra f1-new MiaFinestra(11,12,13);
-f1.setLocation[x:12b0,y:400);
-f1.setSize(width:400,height:200);
-f1.setVisible(b:true);
+MiaFinestra f1=new MiaFinestra(l1,l2,l3);
+f1.setLocation(1280,400);
+f1.setSize(400,200);
+f1.setVisible(true);
   }
 
 }
--- MiaFinestra.java
+++ MiaFinestra.java
@@ -1,23 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
 
-  public class MiaFinestra extends JFramel{
-    private JTextField 11;
-    private JTextArea 12;
-    private JButton 13;
+  public class MiaFinestra extends JFrame{
+    private JTextField l1;
+    private JTextArea l2;
+    private JButton l3;
     private JPanel p1;
 
-MiaFinestra(String 11,String 12,String 13){
+MiaFinestra(String l1,String l2,String l3){
     Container c= this.getContentPane();
-    p1=new JPanel(new FlowLayout(FlowLayout.RIGHT)):
-    this.11-new JTextField(l1);
-    this.12-new JTextArea(12);
-    this.13-new JButton(13);
-    setTitle(title:"Finestra BorderLayout");
-    setLayout (new BorderLayout());
+    p1=new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    this.l1=new JTextField(l1);
+    this.l2=new JTextArea(l2);
+    this.l3=new JButton(l3);
+    setTitle("Finestra BorderLayout");
+    setLayout(new BorderLayout());
     c.add(this.l2,BorderLayout.CENTER);
     c.add(this.l1,BorderLayout.NORTH);
-    pl.add(this.13);
+    p1.add(this.l3);
     c.add(p1,BorderLayout.SOUTH);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
